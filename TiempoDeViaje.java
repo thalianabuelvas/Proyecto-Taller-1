526ef3984d177a3c7b150ddc7a8478fca1e8d104
thalianabuelvas <thalianabuelvasd17@gmail.com>
2026-09-02 20:51:22 -0500

    tiempodeviaje

---
+++ TiempoDeViaje.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TiempoDeViaje{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa tu tiempo de viaje en minutos ");
+        int minutosTotales = sc.nextInt();
+
+        int TiempoHoras = minutosTotales / 60;
+        int MinutosRestantes = minutosTotales % 60;
+        int segundosTotales = minutosTotales * 60;
+
+        System.out.println("Tiempo ingresado: " + minutosTotales + "minutos");
+        System.out.println("Equivale a: " + TiempoHoras + " horas, " + MinutosRestantes + " min");
+        System.out.println("En segundos: " + segundosTotales);
+
+        sc.close();
+
+
+
+
+
+
+    }
+
+}
+    
